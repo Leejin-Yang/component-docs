44b314f48720295db903a5d8bea6b682b96864ed
Leejin-Yang <ejin1996@gmail.com>
2023-09-17 03:47:19 +0900

    feat: 컴포넌트 문서 페이지 구현

---
+++ src/pages/index.md
@@ -0,0 +1,8 @@
+---
+title: Introduction
+hide_table_of_contents: true
+---
+
+# Layout Component HP
+
+재사용 가능한 리액트 레이아웃 컴포넌트 문서입니다.
---
+++ src/theme/ReactLiveScope/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Flex } from 'layout-component-hp';
+// Add react-live imports you need here
+const ReactLiveScope = {
+  React,
+  ...React,
+  Flex,
+};
+export default ReactLiveScope;
