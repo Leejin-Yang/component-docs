51711cd8d16fbe19cf1e208a68edcaa217f97656
Leejin-Yang <ejin1996@gmail.com>
2023-09-18 00:26:05 +0900

    docs: Container, Grid, Flex 문서 작성

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { Flex } from 'layout-component-hp';
+import * as Layout from 'layout-component-hp';
+
+const DecorativeBox = ({ children }) => (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minWidth: '100px',
+      height: '100%',
+      minHeight: '75px',
+      border: '1px solid #1a211e',
+      borderRadius: '4px',
+    }}
+  >
+    {children}
+  </div>
+);
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
-  Flex,
+  DecorativeBox,
+  ...Layout,
 };
 export default ReactLiveScope;
